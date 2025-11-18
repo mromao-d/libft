dd958bf9e31bc40760e6cbbf4c597f2e3910fe6a
Manuel Romão <98095709+mromao-d@users.noreply.github.com>
2025-11-18 18:56:29 +0000

    Add files via upload

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: mromao-s <mromao-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/02 11:46:02 by mromao-s          #+#    #+#             */
-/*   Updated: 2025/11/02 12:15:54 by mromao-s         ###   ########.fr       */
+/*   Updated: 2025/11/18 18:27:32 by mromao-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	size_t	i;
 
 	i = 0;
-	if (!dest && !src)
-		return (NULL);
 	while (i < n)
 	{
 		((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
@@ -27,12 +25,12 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	return (dest);
 }
 
-/* int	main(void) {
-	char src[100] = "This is the source;";
-	char dst[100] = "Dest here;";
+// int	main(void) {
+// 	char src[100] = "This is the source;";
+// 	char dst[100] = "Dest here;";
 
-	printf("dest here: %s\n", dst);
-	ft_memcpy(dst, src, 20);
-	printf("dest here: %s\n", dst);
-	return (0);
-} */
+// 	printf("dest here: %s\n", dst);
+// 	ft_memcpy(NULL, NULL, 20);
+// 	printf("dest here: %s\n", dst);
+// 	return (0);
+// }
